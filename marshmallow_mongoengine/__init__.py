66b6e7350cc8c947e513c9f12bfbef18197c1112
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-02-18 22:21:19 +0100

    Bump version

--- marshmallow_mongoengine/__init__.py
+++ marshmallow_mongoengine/__init__.py
@@ -19,7 +19,7 @@ from marshmallow_mongoengine.convert import (
 )
 from marshmallow_mongoengine.exceptions import ModelConversionError
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 __license__ = 'MIT'
 
 __all__ = [
