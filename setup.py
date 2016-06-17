5ea15f8ac0ca827b931eb673106534ba6bacbf9d
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-06-17 10:52:39 +0200

    Switch version handling to bumpversion

--- setup.py
+++ setup.py
@@ -9,25 +9,6 @@ REQUIRES = (
 )
 
 
-def find_version(fname):
-    """Attempts to find the version number in the file names fname.
-    Raises RuntimeError if not found.
-    """
-    version = ''
-    with open(fname, 'r') as fp:
-        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
-        for line in fp:
-            m = reg.match(line)
-            if m:
-                version = m.group(1)
-                break
-    if not version:
-        raise RuntimeError('Cannot find version information')
-    return version
-
-__version__ = find_version('marshmallow_mongoengine/__init__.py')
-
-
 def read(fname):
     with open(fname) as fp:
         content = fp.read()
@@ -35,7 +16,7 @@ def read(fname):
 
 setup(
     name='marshmallow-mongoengine',
-    version=__version__,
+    version='0.7.4',
     description='Mongoengine integration with the marshmallow '
                 '(de)serialization library',
     long_description=read('README.rst'),
