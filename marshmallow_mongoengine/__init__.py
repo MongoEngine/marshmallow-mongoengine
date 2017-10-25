9a5c9caae3a0857c54c25a822cabc989fce5ff40
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2017-10-25 17:32:11 +0200

    Bump version: 0.8.0 → 0.9.0

--- marshmallow_mongoengine/__init__.py
+++ marshmallow_mongoengine/__init__.py
@@ -26,7 +26,7 @@ from marshmallow_mongoengine.convert import (
 )
 from marshmallow_mongoengine.exceptions import ModelConversionError
 
-__version__ = '0.8.0'
+__version__ = '0.9.0'
 __license__ = 'MIT'
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='marshmallow-mongoengine',
-    version='0.8.0',
+    version='0.9.0',
     description='Mongoengine integration with the marshmallow '
                 '(de)serialization library',
     long_description=read('README.rst'),
