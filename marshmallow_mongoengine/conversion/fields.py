98fba75a4989cf0131ebc518972d3b019ab82be7
Patrick Huck <phuck@lbl.gov>
2020-07-15 01:13:54 -0700

    use EmbeddedDocumentBuilder for ReferenceField
    ReferenceFields (and ListFields thereof) are validated as Nested
    fields in the schema that way

--- marshmallow_mongoengine/conversion/fields.py
+++ marshmallow_mongoengine/conversion/fields.py
@@ -183,7 +183,7 @@ register_field(
     me.fields.GenericEmbeddedDocumentField, ma_fields.GenericEmbeddedDocument
 )
 register_field_builder(me.fields.GenericReferenceField, GenericReferenceBuilder)
-register_field_builder(me.fields.ReferenceField, ReferenceBuilder)
+register_field_builder(me.fields.ReferenceField, EmbeddedDocumentBuilder)
 # LazyReferenceField and GenericLazyReference need mongoengine >= 0.15.0
 if hasattr(me.fields, "LazyReferenceField"):
     register_field_builder(me.fields.LazyReferenceField, ReferenceBuilder)
