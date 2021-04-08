adffa51db85ea1563e6518a921a62f886aa5e58c
Patrick Huck <phuck@lbl.gov>
2021-04-08 11:01:34 -0700

    Bump version: 0.30.0 → 0.30.1

--- marshmallow_mongoengine/__init__.py
+++ marshmallow_mongoengine/__init__.py
@@ -26,7 +26,7 @@ from marshmallow_mongoengine.convert import (
 )
 from marshmallow_mongoengine.exceptions import ModelConversionError
 
-__version__ = "0.30.0"
+__version__ = "0.30.1"
 __license__ = "MIT"
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="marshmallow-mongoengine",
-    version="0.30.0",
+    version="0.30.1",
     description="Mongoengine integration with the marshmallow "
                 "(de)serialization library",
     long_description=read("README.rst"),
