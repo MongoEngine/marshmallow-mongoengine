1dd71318224ecd0355d9ed08c6d2ecaefe30874b
Sergi Pons Freixes <sponsfre@qualcomm.com>
2022-06-02 16:43:05 -0700

    Reformat project with black
    It seems that black was the code formatter originally used (see tox.ini), but
    as we stopped using travis-ci, this was not enforced.
    
    Now that we are starting to use GitHub actions to run linting, we need to fix
    the existing code, which is what this commit does.

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import datetime as dt
-import decimal
-from datetime import datetime
 
 import mongoengine as me
 
-from marshmallow import validate, Schema
+from marshmallow_mongoengine import (
+    ModelSchema,
+)
 
-import pytest
-from marshmallow_mongoengine import (fields, fields_for_model, ModelSchema,
-                                     ModelConverter, convert_field, field_for)
-
-
-TEST_DB = 'marshmallow_mongoengine-test'
+TEST_DB = "marshmallow_mongoengine-test"
 db = me.connect(TEST_DB)
 
 
@@ -25,19 +19,19 @@ class BaseTest(object):
 
 
 class TestSkip(BaseTest):
-
     def test_skip_none_field(self):
         class Doc(me.Document):
-            field_not_empty = me.StringField(default='value')
+            field_not_empty = me.StringField(default="value")
             field_empty = me.StringField()
             list_empty = me.ListField(me.StringField())
 
         class DocSchema(ModelSchema):
             class Meta:
                 model = Doc
+
         doc = Doc()
         dump_data = DocSchema().dump(doc)
-        assert dump_data == {'field_not_empty': 'value'}
+        assert dump_data == {"field_not_empty": "value"}
 
     def test_disable_skip_none_field(self):
         class Doc(me.Document):
@@ -48,6 +42,7 @@ class TestSkip(BaseTest):
             class Meta:
                 model = Doc
                 model_skip_values = ()
+
         doc = Doc()
         dump_data = DocSchema().dump(doc)
-        assert dump_data == {'field_empty': None, 'list_empty': []}
+        assert dump_data == {"field_empty": None, "list_empty": []}
