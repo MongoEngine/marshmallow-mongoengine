8f289a14e9bdd86d2199b11d37b1c45a4526f607
Sergi Pons Freixes <sponsfre@qualcomm.com>
2022-06-02 16:43:05 -0700

    Reformat project with black
    It seems that black was the code formatter originally used (see tox.ini), but
    as we stopped using travis-ci, this was not enforced.
    
    Now that we are starting to use GitHub actions to run linting, we need to fix
    the existing code, which is what this commit does.

--- tests/test_params.py
+++ tests/test_params.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-import datetime as dt
-import decimal
-from datetime import datetime
 
-from marshmallow.exceptions import ValidationError
 import mongoengine as me
-
 import pytest
-from marshmallow_mongoengine import ModelSchema
 from marshmallow.exceptions import ValidationError
 
+from marshmallow_mongoengine import ModelSchema
 
-
-TEST_DB = 'marshmallow_mongoengine-test'
+TEST_DB = "marshmallow_mongoengine-test"
 db = me.connect(TEST_DB)
 
 
@@ -25,7 +19,6 @@ class BaseTest(object):
 
 
 class TestParams(BaseTest):
-
     def test_required(self):
         class Doc(me.Document):
             field_not_required = me.StringField()
@@ -34,18 +27,21 @@ class TestParams(BaseTest):
         class DocSchema(ModelSchema):
             class Meta:
                 model = Doc
+
         with pytest.raises(ValidationError) as excinfo:
-            doc = DocSchema().load({'field_not_required': 'bad_doc'})
-        assert excinfo.value.args[0] == {'field_required': ['Missing data for required field.']}
+            doc = DocSchema().load({"field_not_required": "bad_doc"})
+        assert excinfo.value.args[0] == {
+            "field_required": ["Missing data for required field."]
+        }
         # Now provide the required field
-        doc = DocSchema().load({'field_required': 'good_doc'})
+        doc = DocSchema().load({"field_required": "good_doc"})
         assert doc.field_not_required is None
-        assert doc.field_required == 'good_doc'
+        assert doc.field_required == "good_doc"
 
         # Update should not take care of the required fields
-        doc = DocSchema().update(doc, {'field_not_required': 'good_doc'})
-        assert doc.field_required == 'good_doc'
-        assert doc.field_not_required == 'good_doc'
+        doc = DocSchema().update(doc, {"field_not_required": "good_doc"})
+        assert doc.field_required == "good_doc"
+        assert doc.field_not_required == "good_doc"
 
     def test_required_with_default(self):
         class Doc(me.Document):
@@ -55,61 +51,71 @@ class TestParams(BaseTest):
         class DocSchema(ModelSchema):
             class Meta:
                 model = Doc
+
         doc = DocSchema().load({})
         assert doc.basic == 42
         assert doc.cunning is False
 
     def test_default(self):
         def generate_default_value():
-            return 'default_generated_value'
+            return "default_generated_value"
 
         class Doc(me.Document):
-            field_with_default = me.StringField(default='default_value')
-            field_required_with_default = me.StringField(required=True,
-                default=generate_default_value)
+            field_with_default = me.StringField(default="default_value")
+            field_required_with_default = me.StringField(
+                required=True, default=generate_default_value
+            )
 
         class DocSchema(ModelSchema):
             class Meta:
                 model = Doc
+
         # Make sure default doesn't shadow given values
-        doc = DocSchema().load({'field_with_default': 'custom_value',
-                                'field_required_with_default': 'custom_value'})
-        assert doc.field_with_default == 'custom_value'
-        assert doc.field_required_with_default == 'custom_value'
+        doc = DocSchema().load(
+            {
+                "field_with_default": "custom_value",
+                "field_required_with_default": "custom_value",
+            }
+        )
+        assert doc.field_with_default == "custom_value"
+        assert doc.field_required_with_default == "custom_value"
         # Now use defaults
         doc = DocSchema().load({})
-        assert doc.field_with_default == 'default_value'
-        assert doc.field_required_with_default == 'default_generated_value'
+        assert doc.field_with_default == "default_value"
+        assert doc.field_required_with_default == "default_generated_value"
 
     def test_choices(self):
         class Doc(me.Document):
             CHOICES = (
-                (0, 'zero'),
-                (1, 'one'),
+                (0, "zero"),
+                (1, "one"),
             )
             basic = me.IntField(choices=CHOICES)
 
         class DocSchema(ModelSchema):
             class Meta:
                 model = Doc
-        doc = DocSchema().load({'basic': 0})
+
+        doc = DocSchema().load({"basic": 0})
         assert doc.basic == 0
 
         with pytest.raises(ValidationError) as excinfo:
-            doc = DocSchema().load({'basic': 3})
-        assert excinfo.value.args[0] == {'basic': ['Must be one of: 0, 1.']}
+            doc = DocSchema().load({"basic": 3})
+        assert excinfo.value.args[0] == {"basic": ["Must be one of: 0, 1."]}
 
     def test_regex(self):
         class Doc(me.Document):
-            basic = me.StringField(regex=r'^[1-9]{6}$')
+            basic = me.StringField(regex=r"^[1-9]{6}$")
 
         class DocSchema(ModelSchema):
             class Meta:
                 model = Doc
 
-        doc = DocSchema().load({'basic': '112233'})
-        assert doc.basic == '112233'
+        doc = DocSchema().load({"basic": "112233"})
+        assert doc.basic == "112233"
 
         with pytest.raises(ValidationError) as excinfo:
-            doc = DocSchema().load({'basic': '1A2B3CDD'})
-        assert excinfo.value.args[0] == {'basic': ['String does not match expected pattern.']}
+            doc = DocSchema().load({"basic": "1A2B3CDD"})
+        assert excinfo.value.args[0] == {
+            "basic": ["String does not match expected pattern."]
+        }
